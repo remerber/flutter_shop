c039c783da52b9a2e4972bb84b9c42a9f7fe34a5
wangwei <275281462@qq.com>
2020-02-15 15:59:44 +0800

    09首页项目结构和获取数据

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+import '../service/service_method.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+  @override
+  void initState() {
+    getHomePageContent().then((val) {
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
+      appBar: AppBar(
+        title: Text('百姓生活+'),
+      ),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
       ),
     );
   }
 }
+
