ac3c30225ff69fe677bec431defded49ac575654
wangwei <275281462@qq.com>
2020-02-15 22:18:29 +0800

    10 首页轮播效果制作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../service/service_method.dart';
-
+import 'dart:convert';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -9,24 +10,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String homePageContent = '正在获取数据';
   @override
-  void initState() {
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('百姓生活+'),
       ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
-      ),
+      body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              var data=json.decode(snapshot.data.toString());
+              List<Map> swiper=(data['data']['slides'] as List).cast();
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDateList: swiper)
+                ],
+              );
+            }else{
+              return Center(
+                child: Text('加载中。。。'),
+              );
+            }
+          }),
     );
   }
+
+
 }
+//首页轮播组件
+  class SwiperDiy  extends StatelessWidget {
+  final List swiperDateList;
+
+  const SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        height: 333,
+        child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return Image.network(
+                '${swiperDateList[index]['image']}',
+                fit: BoxFit.fill);
+          },
+          itemCount: swiperDateList.length,
+          pagination: SwiperPagination(),
+          autoplay: true,
+        ),
+
+      );
+    }
+  }
+
+
 
