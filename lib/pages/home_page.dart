7696a31b8dfc642f8ba7ed1b90703a6d694ef972
wangwei <275281462@qq.com>
2020-02-18 17:01:43 +0800

    去掉多余的AutomaticKeepAliveClientMixin。IndexedStack可以保持状态

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,11 +9,10 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
+class _HomePageState extends State<HomePage>  {
 
 
-  @override
-  bool get wantKeepAlive => true;
+
 
   String homePageContent = '正在获取数据';
   @override
