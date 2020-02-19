0ae061e1c1431fbfae5257a52998ec4d5b051dc9
wangwei <275281462@qq.com>
2020-02-19 10:32:16 +0800

    21.分类页_类别信息接口调试 保存json

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
-class CategoryPage extends StatelessWidget {
+import '../service/service_method.dart';
+import 'dart:convert';
+
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    _getCategory();
+    return Container(
+      child: Center(
         child: Text('分类'),
       ),
     );
   }
+
+  void _getCategory() async {
+    await request('getCategory').then((val){
+      var data = json.decode(val.toString());
+      print(data);
+    });
+  }
 }
+
