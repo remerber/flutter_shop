d02e2beaab3092698315a0c42f1968c8f35f5c23
wangwei <275281462@qq.com>
2020-02-20 17:04:21 +0800

    24.Provide状态管理基础

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/pages/index_page.dart';
+import 'package:provide/provide.dart';
+import './provide/counter.dart';
 
+void main() {
+  var counter=Counter();
+  var providers=Providers();
+  providers..provide(Provider<Counter>.value(counter));
+  runApp(ProviderNode(child: MyApp(), providers: providers));
 
-void main() => runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
