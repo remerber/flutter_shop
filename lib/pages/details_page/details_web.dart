3d8e1af83c69d1ad84c1aa427456b6f2435564f6
wangwei <275281462@qq.com>
2020-02-24 22:33:01 +0800

    48.详细页_详情和评论的切换

--- lib/pages/details_page/details_web.dart
+++ lib/pages/details_page/details_web.dart
@@ -11,8 +11,22 @@ class DetailsWeb  extends StatelessWidget {
         .data
         .goodInfo
         .goodsDetail;
-    return Container(
-      child: Html(data: goodsDetails),
+    return Provide<DetailsInfoProvide>(
+       builder: (context,child,val){
+         var isLeft = Provide.value<DetailsInfoProvide>(context).isLeft;
+         if(isLeft){
+           return Container(
+             child: Html(data: goodsDetails)
+           );
+         }else{
+           return Container(
+             width: ScreenUtil().setWidth(750),
+             padding: EdgeInsets.all(10),
+             alignment: Alignment.center,
+             child: Text('暂时没有数据'),
+           );
+         }
+       },
     );
   }
 }
